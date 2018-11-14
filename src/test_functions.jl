c65ffa5b89dd033382f96fa186d52cf74f7de7b6
mstobb <michael@stobb.org>
2018-11-14 13:31:52 -0800

    updating test_functions to 1.0

--- src/test_functions.jl
+++ src/test_functions.jl
@@ -1,4 +1,5 @@
 # List of possible test functions to use
+using Combinatorics
 
 # The Sobol G Function
 
@@ -7,34 +8,35 @@ function sobolG(x::Array{Float64,1}; a = [0.0, 0.0, 0.0, 0.4, 0.4, 5.0])
         error("Inputs are of different lenths!")
     end
 
-    return prod((abs.(4*x - 2) .+ a)./(1 .+ a));
+    return prod((abs.(4*x .- 2) .+ a)./(1 .+ a));
 end
 
 function sobolG_analytic(; a = [0.0, 0.0, 0.0, 0.4, 0.4, 5.0])
     y = zeros(2,length(a))
 
     # Compute Vi, the first order indicies
-    Vi = (1/3).*(1 .+a).^(-2)
+    Vi = (1/3) .* (1 .+ a) .^ (-2)
     y[1,:] = Vi
 
     # Use the Vi to compute the total order index, but first determine all
     # possible combinations of variables:
     I = monomialDegrees(length(a),length(a))
-    I[find(I.>0)] = 1
-    I = unique(I,1)          # All unique combinations
-    J = I[find(I[:,1] .== 1),:]    # First element is non-zero
+    I[(LinearIndices(I))[findall(x->x!=0,I)]] .= 1
+    I = unique(I,dims=1)          # All unique combinations
+    J = I[(LinearIndices(I[:,1]))[findall(x->x==1,I[:,1])],:]    # First element is non-zero
 
+    #println(J)
     # Find the total variance
     Vn = 0;
     for k = 2:size(I,1) # First element is all zero, so skip
-        Vn += prod(Vi[I[k,:].==1]);
+        Vn += prod(Vi[(LinearIndices(I[k,:]))[findall(x->x==1,I[k,:])]]);
     end
 
     for j = 1:length(a)
         # move the ith element to the start
         tempVi = [Vi[j]; Vi]
         deleteat!(tempVi,j+1)
-        println(tempVi)
+        #println(tempVi)
         for k = 1:size(J,1)
             y[2,j] += prod(tempVi[J[k,:].==1])
         end
@@ -75,14 +77,14 @@ function monomialDegrees(numVars::Integer, maxDegree::Integer)
     	# For each degree (starting at 1) up to maxDegree...
     	for n = 1:maxDegree
     	    # Find the integers that divide up the RV permutations
-    	    dividers = flipdim(transpose(hcat(collect(combinations(collect(1:(n+k-1)), k-1))...)),1);
+    	    dividers = reverse(transpose(hcat(collect(combinations(collect(1:(n+k-1)), k-1))...)),dims=1);
     	    div_diff = zeros(Int64,size(dividers,1),size(dividers,2)-1);
     	    for i = 1:size(dividers,1)
     		          div_diff[i,:] = diff(vec(dividers[i,:]));
     	    end
 
     	    # Form the index
-    	    degrees = vcat(degrees, hcat(dividers[:,1] - 1, div_diff -1, (n+k)-dividers[:,end]-1));
+            degrees = vcat(degrees, hcat(dividers[:,1] .- 1, div_diff .- 1, (n+k) .-dividers[:,end] .- 1));
     	end
     	# Output result
     	degrees
